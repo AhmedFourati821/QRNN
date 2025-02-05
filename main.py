e1c45174a009683d0b6931cb93d4b0d115c116d7
ahmed fourati <ahmed.fourati@stud.tu-darmstadt.de>
2025-02-05 22:26:08 +0100

    feat: optimal hyperparameters

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     train_X, train_y, test_X, test_y = get_train_test_data(dataset_name)
 
     # Model Initialization
-    n_qubits = 4
-    n_layers = 6
+    n_qubits = 8
+    n_layers = 4
     input_dim = 100  # GloVe vectors are 100D
     model = QuantumRNN(n_qubits=n_qubits, input_dim=input_dim, n_layers=n_layers)
 
     # Train the Quantum RNN
     train_quantum_rnn(
-        model, train_X, train_y, test_X, test_y, epochs=30, batch_size=32, lr=0.01
+        model, train_X, train_y, test_X, test_y, epochs=30, batch_size=16, lr=0.03
     )
